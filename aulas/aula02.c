0026a0c334a740b24d607c9c1936c9ef97af0e3a
Thiago José Franchini <thiagojfranchini@gmail.com>
2022-05-12 02:35:25 -0300

    Create aula02.c
    declaração de variáveis

---
+++ aulas/aula02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    char letra = 'c'; /* declaração de string */
+
+    int nro = 13; /* declaração de número inteiro*/
+
+    float num = 3.56; /* declaração de número com ponto flutuante atentar que é ponto flutuante e não vírgula flutuante, ou seja fica entre +/- 8 388 607 */
+
+    double mol = 6e23; /* declaração de número com ponto flutuante, mas de 64bit, sendo 53 para mantissa, ou seja fica entre +/- 4 503 599 627 370 495 */
+   
+    return 0;
+}
