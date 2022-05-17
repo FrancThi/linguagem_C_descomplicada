6ad5581ee0eae5842faca2d6b6faedf2fea6d622
Thiago José Franchini <thiagojfranchini@gmail.com>
2022-05-17 13:29:00 -0300

    Create aula31.c

---
+++ Conteúdo/05 - Strings/aula31.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    char palavra0[20], palavra1[20];
+    printf("Digite uma palavra: ");
+    scanf("%s", palavra0); //termina a leitura no momento q o usuário teclar "espaço" ou "entrar", se o usuário teclar "espaço" e continuar teclando, a linguagem ignora o que tiver a partir do "espaço"
+    //gets(palavra); // termina a leitura apenas qdo o usuário teclar "entrar"
+    printf("Palavra lida: %s\n", palavra0);
+
+    printf("Digite uma frase: ");
+    gets(palavra1); //muitas pessoas não indicar trabalhar com o comando "gets()" por conter falhas de seguraça entre outros.
+
+    char nome0[20] = {'J', 'o', 'ã', 'o', '\0'}; // atentar para o "\0" no final, ele que indica q é o último elemento do array
+    printf("\nnome: %s\n", nome0);
+    //Forma especial
+
+    char nome1[20] = "João";
+    printf("nome: %s\n", nome1);
+    
+    system('pause');
+    return 0;
+}
