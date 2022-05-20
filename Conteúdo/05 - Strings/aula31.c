39c44da874ef52042affedaec06eaa14f6c12846
Thiago José Franchini <thiagojfranchini@gmail.com>
2022-05-19 22:18:12 -0300

    Update aula31.c

--- Conteúdo/05 - Strings/aula31.c
+++ Conteúdo/05 - Strings/aula31.c
@@ -9,17 +9,29 @@ int main(int argc, char const *argv[])
     //gets(palavra); // termina a leitura apenas qdo o usuário teclar "entrar"
     printf("Palavra lida: %s\n", palavra0);
 
-    printf("Digite uma frase: ");
-    gets(palavra1); //muitas pessoas não indicar trabalhar com o comando "gets()" por conter falhas de seguraça entre outros.
+    //printf("Digite uma frase: ");
+    //gets(palavra1); //muitas pessoas não indicar trabalhar com o comando "gets()" por conter falhas de seguraça entre outros.
 
     char nome0[20] = {'J', 'o', 'ã', 'o', '\0'}; // atentar para o "\0" no final, ele que indica q é o último elemento do array
     printf("\nnome: %s\n", nome0);
     //Forma especial
 
     char nome1[20] = "João";
-    printf("nome: %s\n", nome1);
+    printf("nome: %s\n\n", nome1);
     
+    char palavra2[20] = "Teste";
+    printf("Palavra = %s\n", palavra2);
+    palavra2[0] = 'L'; // alterando a primeira posição pelo char 'L'
+    printf("Palavra = %s\n\n", palavra2);
+
+    char palavra3[20] = "Oitenta";
+    printf("Palavra: %s\n", palavra3);
+
+    palavra3[2] = '\0'; //substituindo a terceira posição pelo "fim do string/vetor/matriz" e a partir disso ele ignora tudo o q vier depois da terceira posição
+    printf("Palavra: %s\n", palavra3);
+    //printf("Quarta posição: %s\n", palavra3[3]); // como prova, aqui ele retorna erro
+    printf("Terceira posição: %s\n", palavra3[2]); // e aqui ele retorna "null"
+
     system('pause');
-    
     return 0;
 }
