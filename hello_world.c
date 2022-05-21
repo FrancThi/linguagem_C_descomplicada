957b94bc5361b471c4a699cace18e3825cab8358
Thiago José Franchini <thiagojfranchini@gmail.com>
2022-05-21 15:47:24 -0300

    git ignore
    Aumentando a lista do .gitignore
    
    fiz um programinha no hello_wolrd.c teste mesmo, apenas pra não precisar digitar aula por aula

--- hello_world.c
+++ hello_world.c
@@ -4,7 +4,11 @@
 int main(int argc, char const *argv[])
 {
     printf("Hello world!\n");
-        
+    
+    //gerando o nome das linhas pra colocar no .gitignore ;)
+    short int i;
+    for (i=1; i<=200; i++) printf("aula%d\n", i);
+
     system('pause');
     return 0;
 }
