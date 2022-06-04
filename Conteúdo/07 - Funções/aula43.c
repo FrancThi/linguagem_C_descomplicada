37a89118d237f0971e600437b57a15cbe9acdd23
Thiago José Franchini <thiagojfranchini@gmail.com>
2022-06-04 11:18:26 -0300

    Update aula43.c

--- Conteúdo/07 - Funções/aula43.c
+++ Conteúdo/07 - Funções/aula43.c
@@ -1,30 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int quadrado(int a){return a*a;} //exemplo de função
 
 int cubo(int); //declaração de protótipo de função. Obs.: obrigatório caso a função esteja descrita despois da main, ou seja, ela é "escrita" lá, e "prototipada" antes da main
  
-int raiz(int); //pode ser declarada sem a "variável declarada"
-
 int main(int argc, char const *argv[])
 {
-    int x;
+    int x, y;
+    printf("Digite x: ");
     scanf("%d",&x);
     // as funções scanf e printf são exemplos de funções e foram registradas na biblioteca stdio.h
-    x = quadrado(x);
-    printf("%d\n", x);
+    y = quadrado(x);
+    printf("x² = %d\n", y);
     
-    x = raiz(x);
-    printf("%d\n", x);
-
-    x = cubo(x);
-    printf("%d\n", x);
+    y = cubo(x);
+    printf("x³ = %d\n", y);
 
     system('pause');
     return 0;
 }
 
 int cubo(int a){return a*a*a;} // função declarada depois da main. Obs.: obrigatório invocar um protótipo desta função antes da main
-
-int raiz(int a){return a^(1/2);}
