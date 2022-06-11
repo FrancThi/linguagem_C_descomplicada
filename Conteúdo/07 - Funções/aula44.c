7ac3f269fc9f7681c3f85d20bf8f0bb753df30ad
Thiago José Franchini <thiagojfranchini@gmail.com>
2022-06-11 19:01:16 -0300

    Update aula44.c
    honestamente é difícil compreender bem a aula qdo o programa não roda, quero rever posteriormente o pq desses erros

--- Conteúdo/07 - Funções/aula44.c
+++ Conteúdo/07 - Funções/aula44.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
 float raiz(float r);
 int soma(int x, int y);
+*/
 
 int main(int argc, char const *argv[])
 {
-    /*
+/*
     //int y;
     float raiz(float r);
     int soma (int x, int y);
     //int soma (int x, y); ERRADO, tem de ser parametrizado ambas as variáveis (a não ser que ela já havia sido previamente informada)
     float exponencial(float a, int b); // repare que posso atribuir diferentes parâmetros para diferentes variáveis.
-    */
+*/
 
+/*
     float k, w = 5.4;
     k = raiz(w);
     k = raiz(5.4);
@@ -24,6 +27,18 @@ int main(int argc, char const *argv[])
     x = soma(y,z);
     x = soma(1,2);
     x = soma(y,2);
+*/
+    float leitura(); //função sem parâmetros ignorando tudo o q há dentro dos parênteses no momento da infocação
+    int le_arquivo(void); //função sem parâmetros que devolver erro se no momento da invocação constar algum argumento nos parâmetros
+
+    float y;
+    y = leitura();
+    y = leitura(5); //um parâmetro ignorado
+    y = leitura(5, 'x'); //dois parâmetros ignorados
+
+    int x;
+    x = le_arquivo();
+    // x = le_arquivo(5); ERRO, essa função não pode receber parâmetros
 
     system('pause');
     return 0;
